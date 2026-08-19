1a5af2e734f24dd35f3e94c0ac177d40d66578bd
Chahn <chahna12@gmail.com>
2026-08-19 15:45:12 +0530

    Complete Day 1

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("WELCOME TO BAND NAME GENERATOR..")
+city_input = input("which city did you grew up in? \n")
+pet_name = input("what is you pet's name \n")
+print("your BAND NAME could be : " + city_input + " " + pet_name)
